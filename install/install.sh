1ec26aaaf8a4aa9207fd089349bdc9bada3121b9
TJEDII <tjdawkins@redtreeit.com>
2016-02-08 15:04:47 -0800

    Update install script

---
+++ install/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Make the data directory 
+mkdir /home/root/data
+
+# Setup bluetooth and sketch starup
+if [ ! -d /etc/init.d]; then
+	mkdir /etc/init.d
+fi
+
+cp ./enableBT.sh /etc/init.d
+cp ./startSketch.sh /etc/init.d
+
+chmod +x /etc/init.d/enableBT.sh
+chmod +x /etc/init.d/startSketch.sh
+
+cd /etc/init.d
+
+update-rc.d enableBT.sh default
+update-rc.d startSketch.sh default
