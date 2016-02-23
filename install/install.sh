25be45d284842c49df68d28aa314391fddff9893
tdawkins <tjdawkins@redtreeit.com>
2016-02-23 12:15:36 -0800

    Update install file for start SPP.py

--- install/install.sh
+++ install/install.sh
@@ -10,11 +10,14 @@ fi
 
 cp ./enableBT.sh /etc/init.d
 cp ./startSketch.sh /etc/init.d
+cp ./startSPP.sh /etc/init.d
 
 chmod +x /etc/init.d/enableBT.sh
 chmod +x /etc/init.d/startSketch.sh
+chmod +x /etc/init.d/startSPP.sh
 
 cd /etc/init.d
 
 update-rc.d enableBT.sh default
 update-rc.d startSketch.sh default
+update-rc.d startSPP.sh default
